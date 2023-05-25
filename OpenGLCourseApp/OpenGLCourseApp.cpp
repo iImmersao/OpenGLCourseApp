353e27ea896229952654d0d259650e38b20ac280
iImmersao <philippatchin@gmail.com>
2023-05-25 10:59:23 -0400

    Adding textures.

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -1,5 +1,6 @@
 // OpenGLCourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define STB_IMAGE_IMPLEMENTATION
 
 #include <iostream>
 
@@ -20,6 +21,7 @@
 #include "Shader.h"
 #include "Window.h"
 #include "Camera.h"
+#include "Texture.h"
 
 // Creating identity matrices:
 // Old style:
@@ -33,6 +35,9 @@ Window mainWindow;
 std::vector<Mesh*> meshList;
 std::vector<Shader> shaderList;
 Camera camera;
+
+Texture brickTexture;
+Texture dirtTexture;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastTime = 0.0f;
@@ -52,18 +57,19 @@ void CreateObjects() {
     };
 
     GLfloat vertices[] = {
-        -1.0f, -1.0f, 0.0f,
-        0.0f, -1.0f, 1.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f
+    //  x       y       z       u       v
+        -1.0f, -1.0f, 0.0f,     0.0f,   0.0f,
+        0.0f, -1.0f, 1.0f,      0.5f,   0.0f,
+        1.0f, -1.0f, 0.0f,      1.0f,   0.0f,
+        0.0f, 1.0f, 0.0f,       0.5f,   1.0f
     };
 
     Mesh* obj1 = new Mesh();
-    obj1->CreateMesh(vertices, indices, 12, 12);
+    obj1->CreateMesh(vertices, indices, 20, 12);
     meshList.push_back(obj1);
 
     Mesh* obj2 = new Mesh();
-    obj2->CreateMesh(vertices, indices, 12, 12);
+    obj2->CreateMesh(vertices, indices, 20, 12);
     meshList.push_back(obj2);
 }
 
@@ -82,6 +88,11 @@ int main()
     CreateShaders();
 
     camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), 90.0f, 0.0f, 5.0f, 1.0f);
+
+    brickTexture = Texture("Textures/brick.png");
+    brickTexture.LoadTexture();
+    dirtTexture = Texture("Textures/dirt.png");
+    dirtTexture.LoadTexture();
 
     GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0;
 
@@ -117,12 +128,14 @@ int main()
         glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
 
+        brickTexture.UseTexture();
         meshList[0]->RenderMesh();
 
         model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(0.0f, 1.0f, -2.5f));
         model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        dirtTexture.UseTexture();
         meshList[1]->RenderMesh();
 
         glUseProgram(0);
